02e7a4ddbb6b4ba6ec8ce150050ee0284c7bf8bd
user2 <user2@KH_C>
2019-02-07 20:58:11 +0900

    정연아 안녕

--- src/com/kh/test.java
+++ src/com/kh/test.java
@@ -5,7 +5,7 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println("Hello Wolrd");
 		System.out.println("Hello Wolrd2");
-
+		System.out.println("jeongyeon babo");
 	}
 
 }
